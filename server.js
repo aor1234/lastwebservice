657fd299a3d3eb6e45cad7e5c554e2d5083e3750
aor1234 <58202304+aor1234@users.noreply.github.com>
2020-01-20 11:21:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -23,21 +23,21 @@ app.get('/', function(req, res, next) {
 
 
 app.post('/test', function(req, res, next) {
-//         var options = {
-//         method: 'POST',
-//         uri: 'https://80beac66.ngrok.io/test',
-//         json: true
-//     };
+        var options = {
+        method: 'POST',
+        uri: 'https://31f523c5.ngrok.io/test',
+        json: true
+    };
 
-//     rp(options)
-//         .then(function(parsedBody) {
-//             res.send(parsedBody)
-//         })
-//         .catch(function(err) {
-//             return next(err);
-//         });
+    rp(options)
+        .then(function(parsedBody) {
+            res.send(parsedBody)
+        })
+        .catch(function(err) {
+            return next(err);
+        });
     
-    res.send("aor");
+    
 });
 
 app.listen(process.env.PORT || port, function() {
